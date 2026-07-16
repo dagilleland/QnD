14e7b55ebfb0700a722b551392cceed3b558615e
Dan Gilleland <dagilleland@shaw.ca>
2026-07-15 19:14:57 -0600

    case-sensitive-base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gilleland.ca/',
-	base: '/qnd',
+	base: '/QnD',
 	integrations: [
 		starlight({
 			title: 'My Docs',
